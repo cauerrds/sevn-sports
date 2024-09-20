a8ef1761e6da588d4b3b0b8f4d72e70fe0827ba0
cauerrds <caue.rrds@gmail.com>
2024-09-20 01:14:14 -0300

    feat: implement CustomHeader component
    - Create a CustomHeader component for consistent header design

---
+++ src/components/CustomHeader/CustomHeader.js
@@ -0,0 +1,51 @@
+
+export class CustomHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+
+  render() {
+    const mainTitle = this.getAttribute('main-title');
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        .container {
+            width: 100%;
+            background-color: #33B667;
+            height: 76px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            column-gap: 4px;
+            color: #FFFFFF
+        }
+
+        .main-title {
+            margin: 0;
+            font-size: 22px;
+            line-height: 26.63;
+        }
+            
+        .container > img {
+            height: 22px;
+        }
+      </style>
+
+      <div class="container">
+          <h1 class="main-title">${mainTitle}</h1>
+          <img src="./assets/images/ball.svg"  alt="Soccer Ball">
+      </div>
+  `;
+  }
+
+
+}
+
+customElements.define('custom-header', CustomHeader);
